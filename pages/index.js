5d65ed339c5b28079ed9514f8a814c54de05e8c2
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-26 15:45:55 -0300

    Home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
+import Image from 'next/image'
+import Widget from '../src/components/Widget'
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const Wrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  min-height: 100vh;
 `
 
+const ImageContainer = styled.div`
+  width: 100vh;
+  height: 100vh;
+  position: relative;
+`
+
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Wrapper>
+      <Widget />
+      <ImageContainer>
+        <Image 
+          src='/graduation.jpg'
+          layout="fill"
+          alt="Imagem de estudantes graduados"
+          title="Imagem de estudantes graduados"
+        />
+      </ImageContainer>
+    </Wrapper>
+  )
 }
