4c9853d1fd95e5939bf959ddbd4f7a2668926c15
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-27 15:17:40 -0300

    Change idea !

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,34 @@
-import Image from 'next/image'
-import Widget from '../src/components/Widget'
-import styled from 'styled-components'
-
-const Wrapper = styled.section`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  min-height: 100vh;
-`
-
-const ImageContainer = styled.div`
-  width: 100vh;
-  height: 100vh;
-  position: relative;
-`
-
+import Image from 'next/image';
+import styled from 'styled-components';
+import React from 'react';
 
 export default function Home() {
+  const Wrapper = styled.section`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    min-height: 100vh;
+  `;
+
+  const ImageContainer = styled.div`
+    width: 100vh;
+    height: 100vh;
+    position: relative;
+  `;
+
   return (
     <Wrapper>
-      <Widget />
       <ImageContainer>
-        <Image 
-          src='/graduation.jpg'
+        <Image
+          src="/graduation.jpg"
           layout="fill"
           alt="Imagem de estudantes graduados"
           title="Imagem de estudantes graduados"
         />
       </ImageContainer>
     </Wrapper>
-  )
+  );
 }
+
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,15 +1,16 @@
-import * as S from './styled'
+import React from 'react';
+import * as S from './styled';
 
 export default function Widget() {
   return (
     <S.Wrapper>
       <S.Box>
         <S.Title>Questões de vestibulares</S.Title>
-        <S.QuizLink href="/" title='Ciências da natureza'>Ciências da natureza</S.QuizLink>
-        <S.QuizLink href="/" title='Matemática'>Matemática</S.QuizLink>
-        <S.QuizLink href="/" title='Linguagens e códigos'>Linguagens e códigos</S.QuizLink>
-        <S.QuizLink href="/" title='Ciências humanas'>Ciências humanas</S.QuizLink>
+        <S.QuizLink href="/" title="Ciências da natureza">Ciências da natureza</S.QuizLink>
+        <S.QuizLink href="/" title="Matemática">Matemática</S.QuizLink>
+        <S.QuizLink href="/" title="Linguagens e códigos">Linguagens e códigos</S.QuizLink>
+        <S.QuizLink href="/" title="Ciências humanas">Ciências humanas</S.QuizLink>
       </S.Box>
     </S.Wrapper>
-  )
+  );
 }
