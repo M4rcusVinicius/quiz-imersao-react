8839806a68619289d753fb803425d03ec0cb1964
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-27 08:34:22 -0300

    Widget dsign

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -5,10 +5,10 @@ export default function Widget() {
     <S.Wrapper>
       <S.Box>
         <S.Title>Questões de vestibulares</S.Title>
-        <S.QuizLink href="/">Ciências da natureza</S.QuizLink>
-        <S.QuizLink href="/">Matemática</S.QuizLink>
-        <S.QuizLink href="/">Linguagens e códigos</S.QuizLink>
-        <S.QuizLink href="/">Ciências humanas</S.QuizLink>
+        <S.QuizLink href="/" title='Ciências da natureza'>Ciências da natureza</S.QuizLink>
+        <S.QuizLink href="/" title='Matemática'>Matemática</S.QuizLink>
+        <S.QuizLink href="/" title='Linguagens e códigos'>Linguagens e códigos</S.QuizLink>
+        <S.QuizLink href="/" title='Ciências humanas'>Ciências humanas</S.QuizLink>
       </S.Box>
     </S.Wrapper>
   )
--- src/components/Widget/styled.js
+++ src/components/Widget/styled.js
@@ -5,55 +5,62 @@ import Link from 'next/link'
 const borderRadius = db.theme.borderRadius
 
 export const Wrapper = styled.section`
-  width: 20rem;
+  width: 22rem;
 `
 
 export const Box = styled.div`
-  width: 100%;
-  border: solid 2px black;
-  border-radius: ${borderRadius};
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  box-shadow: 2px 2px 10px ${db.theme.colors.shadow};
+  padding-bottom: 0.5rem;
+  border-radius: ${borderRadius};
 `
 
 export const Title = styled.h1`
-  font-size: 1.5rem;
+  font-size: 1.7rem;
   background-color: blue;
   color: ${db.theme.colors.light};
-  border-radius: ${borderRadius} ${borderRadius} 0 0;
   width: 100%;
-  padding: 0.5rem;
   text-align: center;
+  margin: auto;
+  margin-bottom: 0.5rem;
+  padding: 0.8rem 0;
+  border-radius: ${borderRadius} ${borderRadius} 0 0;
 `
 
-export const QuizLink = ({children, href}) => {
+export function QuizLink({ href, title }) {
   const LinkText = styled.a`
+    color: ${db.theme.colors.text};
+    font-size: 1.4rem;
     text-decoration: none;
+    text-align: center;
+    margin: 0 1rem;
+    margin: auto;
   `
-  const LinkBox = styled.div`
-    text-decoration: none;
+  const LinkContainer = styled.div`
+    padding: 0.5rem;
+    box-shadow: 1px 1px 5px ${db.theme.colors.shadow};
+    margin: 0.5rem;
+    width: 19rem;
+    display: flex;
+    align-items: center;
+    border-radius: ${borderRadius};
+    cursor: pointer;
+    transition: 200ms;
+
+    &:hover {
+      translate: 2px -2px;
+    }
   `
-  return(
-    <LinkBox>
-      <Link href={href} passHref>
-        <LinkText>{children}</LinkText>
-      </Link>
-    </LinkBox>
+  return (
+    <Link href={href} passHref>
+      <LinkContainer>
+        <LinkText>{title}</LinkText>
+      </LinkContainer>
+    </Link>
   )
 }
-
-export const box2 = styled.div`
-  background-color: blue;
-  border-radius: ${borderRadius};
-  width: 100%;
-`
-
-export const box3 = styled.div`
-  background-color: blue;
-  border-radius: ${borderRadius};
-  width: 100%;
-`
 
 
