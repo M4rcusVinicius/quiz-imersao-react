f5a6aa22ee759eb2584c0d0cd3394da339067286
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-01-27 23:11:16 -0300

    Useect react form input

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,11 +1,28 @@
+/* eslint-disable react/jsx-no-bind */
+import { PlayFill } from '@styled-icons/bootstrap/PlayFill';
 import React from 'react';
 import * as S from './styled';
 
 export default function Template() {
+  const [name, setName] = React.useState('');
+  console.log('UseState return', name);
+
   return (
-    <S.Wrapper onSubmit="">
-      <S.NameInput placeholder="Digite seu nome" />
-      <S.Button>Button</S.Button>
+    <S.Wrapper
+      onSubmit={function (info) {
+        info.preventDefault();
+        console.log('Submit with react and next', info);
+      }}
+    >
+      <S.NameInput
+        onChange={function (info) {
+          setName(info.target.value);
+        }}
+        placeholder="Digite seu nome"
+      />
+      <S.Button type="submit" disabled={name.length === 0}>
+        <PlayFill size="38" title="Unlock account" />
+      </S.Button>
     </S.Wrapper>
   );
 }
--- src/components/Form/styled.js
+++ src/components/Form/styled.js
@@ -4,16 +4,32 @@ import theme from '../../dataBase/theme.json';
 export const Wrapper = styled.form`
   width: 100%;
   height: 2.5rem;
+  display: flex;
+  align-items: center;
+  margin-top: 2rem;
 `;
 
 export const NameInput = styled.input`
-  height: 100%;
-  padding: 0 1.2rem;
+  height: 2.8rem;
+  padding: 0 0.8rem;
+  margin: 0;
   font-size: 1.2rem;
   border-radius: ${theme.box.borderRadius} 0 0 ${theme.box.borderRadius};
   border: ${theme.box.border};
 `;
 
 export const Button = styled.button`
-  height: 100%; 
+  height: 2.9rem;
+  border-radius: 0 ${theme.box.borderRadius} ${theme.box.borderRadius} 0;
+  border: ${theme.box.border};
+  border-left: none;
+  color: ${theme.colors.light};
+  background-color: ${theme.colors.primary};
+  cursor: pointer;
+  transition: 200ms;
+
+  &:disabled {
+    background-color: ${theme.colors.dark};
+    cursor: default;
+  }
 `;
